4d27afc0e633893fb6d4e8a6ed188d84459e1f48
Juan Asher <bleaknight@Juans-Mac.local>
2014-04-27 23:15:30 +1200

    added a test class

---
+++ Git Test/bskTest.h
@@ -0,0 +1,13 @@
+//
+//  bskTest.h
+//  Git Test
+//
+//  Created by Juan Asher on 27/04/14.
+//  Copyright (c) 2014 booshaka. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface bskTest : NSObject
+
+@end
---
+++ Git Test/bskTest.m
@@ -0,0 +1,13 @@
+//
+//  bskTest.m
+//  Git Test
+//
+//  Created by Juan Asher on 27/04/14.
+//  Copyright (c) 2014 booshaka. All rights reserved.
+//
+
+#import "bskTest.h"
+
+@implementation bskTest
+
+@end
